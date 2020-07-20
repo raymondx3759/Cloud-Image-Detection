5450586b743a6cbcd4cac54e9df8d8095e7e4760
raymondx3759 <60167421+raymondx3759@users.noreply.github.com>
2020-07-19 21:36:32 -0400

    global constants for modularity

---
+++ constants.py
@@ -0,0 +1,7 @@
+""" Constants file """
+
+resizeX = 0.2
+resizeY = 0.2
+figX = 20
+figY = 8
+imStrings = ['stitched', 'contrast', 'gray', 'gblur', 'highP', 'thresh1', 'mask', 'opened', 'closed', 'im']
